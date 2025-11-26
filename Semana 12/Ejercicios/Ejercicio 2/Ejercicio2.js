dc93e0840a300b6f7ebb2d0bec31c04400dc66c2
IgnacioAScotto <scottoignacioagustin@gmail.com>
2025-11-26 19:54:53 -0300

    Ejercicio 1 y 2 terminados

---
+++ Semana 12/Ejercicios/Ejercicio 2/Ejercicio2.js
@@ -0,0 +1,9 @@
+let nombre = "Ignacio";
+let edad = 21;
+let esEstudiante = true;
+
+console.log('Nombre:', nombre);
+console.log('Edad:', edad);
+console.log('Es estudiante:', esEstudiante);
+
+console.log('Nommbre', nombre, 'Edad', edad, 'Es estudiante', esEstudiante);
