30a201bd7f98f33ee0a0a8f3e102f1841427e54e
IgnacioAScotto <scottoignacioagustin@gmail.com>
2025-11-26 19:59:00 -0300

    Ejercicio 3 terminado

---
+++ Semana 12/Ejercicios/Ejercicio 3/Ejercicio3.js
@@ -0,0 +1,9 @@
+let suma = 5 + 3;
+let resta = 12 - 9;
+let multiplicacion = 3 * 3;
+let division = 16 / 4;
+
+console.log('Suma:', suma);
+console.log('Resta:', resta);
+console.log('Multiplicación:', multiplicacion);
+console.log('División:', division);
